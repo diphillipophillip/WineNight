7b3da2e19c0e88d80453f4a773def04090a2eb57
diphillipophillip <38082718+diphillipophillip@users.noreply.github.com>
2021-06-02 12:25:08 -0400

    All Model Binding Finished for forms

--- FrontEnd/src/UserComponents/multi-ingredient-pairing/multi-ingredient-pairing.component.ts
+++ FrontEnd/src/UserComponents/multi-ingredient-pairing/multi-ingredient-pairing.component.ts
@@ -7,7 +7,61 @@ import { AdminService } from 'src/Services/admin.service';
   styleUrls: ['./multi-ingredient-pairing.component.css']
 })
 export class MultiIngredientPairingComponent implements OnInit {
-  constructor() {}
+  categories = [];
+  firstCategoryChoice;
+  firstSpecialties = [];
+  firstSpecialtyChoice;
+  firstTypes = [];
 
-  ngOnInit(): void {}
+  secondCategoryChoice;
+  secondSpecialties = [];
+  secondSpecialtyChoice;
+  secondTypes = [];
+
+  thirdCategoryChoice;
+  thirdSpecialties = [];
+  thirdSpecialtyChoice;
+  thirdTypes = [];
+
+  constructor(private service: AdminService) {}
+
+  ngOnInit(): void {
+    this.service.getCategories().subscribe(data => {
+      this.categories = data;
+    });
+  }
+
+  onFirstCategoryChange() {
+    this.firstSpecialties = this.firstCategoryChoice.specialties;
+  }
+
+  onFirstSpecialtyChange() {
+    let specialty = this.firstSpecialtyChoice.name;
+
+    this.service.getTypes({ specialty: specialty }).subscribe(data => {
+      this.firstTypes = data.types;
+    });
+  }
+
+  onSecondCategoryChange() {
+    this.secondSpecialties = this.secondCategoryChoice.specialties;
+  }
+
+  onSecondSpecialtyChange() {
+    let specialty = this.secondSpecialtyChoice.name;
+    this.service.getTypes({ specialty: specialty }).subscribe(data => {
+      this.secondTypes = data.types;
+    });
+  }
+
+  onThirdCategoryChange() {
+    this.thirdSpecialties = this.thirdCategoryChoice.specialties;
+  }
+
+  onThirdSpecialtyChange() {
+    let specialty = this.thirdSpecialtyChoice.name;
+    this.service.getTypes({ specialty: specialty }).subscribe(data => {
+      this.thirdTypes = data.types;
+    });
+  }
 }
